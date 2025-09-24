b86a71d9fae0eae4b31d580791c2a3185349740a
sahilhaq2003 <sahilhaq2003@gmail.com>
2025-09-24 10:01:45 +0530

    added onboarding 1

--- app/src/main/java/com/sahil/pulseup/MainActivity.kt
+++ app/src/main/java/com/sahil/pulseup/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.sahil.pulseup
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val nextButton = findViewById<Button>(R.id.nextButton)
+        nextButton.setOnClickListener {
+            val intent = Intent(this, Onboarding1::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/sahil/pulseup/Onboarding1.kt
+++ app/src/main/java/com/sahil/pulseup/Onboarding1.kt
@@ -1,6 +1,8 @@
 package com.sahil.pulseup
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,17 @@ class Onboarding1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_onboarding1)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.second_main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val nextButton2 = findViewById<Button>(R.id.nextButton2)
+        nextButton2.setOnClickListener {
+            val intent = Intent(this, Onboarding2::class.java)
+            startActivity(intent)
         }
     }
 }
